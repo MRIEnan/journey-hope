24490c3cc90f6f232255a5540a16fb98605752f8
MRIenan <enan.mainur@gmail.com>
2022-05-21 23:19:22 +0600

    journey begins

---
+++ pages/play.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HexGridDemo from '../components/Grid';
+// import HexGridDemo from "./Grid.js";
+
+const play = () => {
+    return (
+        <div className="App">
+          <HexGridDemo />
+        </div>
+      );
+};
+
+export default play;
