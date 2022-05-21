c3139e490fbfb34b9fed6cfe98fb67bae18e1a57
MRIenan <enan.mainur@gmail.com>
2022-05-22 04:25:20 +0600

    position cracked

--- components/Grid.js
+++ components/Grid.js
@@ -1,20 +1,80 @@
 import times from "lodash/times";
-import React from "react";
+import React, { useState } from "react";
 import HexagonGrid from "./hexagongrid.js";
 
 const HexGridDemo = () => {
+  const [finalPosition,setFinalPosition] = useState(696);
+  const [initialPosition,setInitialPosition] = useState(696);
   const getHexProps = (hexagon) => {
-    return {
-      style: {
-        fill: "transparent",
-        stroke: "#007aff"
-      },
-      /* style: {
-        fill: "#007aff",
-        stroke: "white"
-      }, */
-      onClick: () => alert(`Hexagon n.${hexagon} has been clicked`)
-    };
+    if(hexagon == finalPosition){
+      return {
+        style: {
+          fill: "rgb(0, 255, 21)",
+          // fill: hexagon==initialPosition?"rgb(250, 125, 101)":"transparent",
+          stroke: "#007aff",
+        },
+        /* style: {
+          fill: "#007aff",
+          stroke: "white"
+        }, */
+        onClick: () => {
+          if(hexagon!=finalPosition){
+            setInitialPosition(finalPosition);
+            setFinalPosition(hexagon);
+          }
+          // if(initialPosition){
+            // setInitialPosition(hexagon);
+            // setFinalPosition(initialPosition);
+            // }
+            // alert(`Hexagon n.${hexagon} has been clicked`)
+        }
+      };
+    }
+    else if(hexagon == initialPosition){
+      return {
+        style: {
+          // fill: hexagon==finalPosition?"rgb(0, 255, 21)":"transparent",
+          fill: "rgb(250, 125, 101)",
+          stroke: "#007aff"
+        },
+        /* style: {
+          fill: "#007aff",
+          stroke: "white"
+        }, */
+        onClick: () => {
+          setInitialPosition(finalPosition);
+          setFinalPosition(hexagon);
+          // if(initialPosition){
+            // setInitialPosition(hexagon);
+            // setFinalPosition(initialPosition);
+          // }
+          // alert(`Hexagon n.${hexagon} has been clicked`)
+        }
+      };
+    }
+    else{
+      return {
+        style: {
+          fill: "transparent",
+          // fill: hexagon==finalPosition?"rgb(0, 255, 21)":"transparent",
+          // fill: hexagon==initialPosition?"rgb(250, 125, 101)":"transparent",
+          stroke: "#007aff"
+        },
+        /* style: {
+          fill: "#007aff",
+          stroke: "white"
+        }, */
+        onClick: () => {
+          setInitialPosition(finalPosition);
+          setFinalPosition(hexagon);
+          // if(initialPosition){
+            // setInitialPosition(hexagon);
+            // setFinalPosition(initialPosition);
+          // }
+          // alert(`Hexagon n.${hexagon} has been clicked`)
+        }
+      };
+    }
   };
 
   const renderHexagonContent = (hexagon) => {
@@ -24,7 +84,7 @@ const HexGridDemo = () => {
         y="50%"
         fontSize={100}
         fontWeight="lighter"
-        style={{ fill: "white" }}
+        style={{ fill: "black" }}
         textAnchor="middle"
       >
         {hexagon}
